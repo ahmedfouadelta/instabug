4e837685be80dee6141c8be0db1fefecde40ebe3
Ahmed Fouad <ahmedfouadelta@gmail.com>
2022-08-11 22:55:39 +0200

    add schema to itignore

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220811191010) do
+ActiveRecord::Schema.define(version: 20220811184734) do
 
   create_table "applications", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "name"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 20220811191010) do
     t.datetime "updated_at",           null: false
     t.integer  "chat_id"
     t.index ["chat_id"], name: "index_messages_on_chat_id", using: :btree
-    t.index ["creation_number", "chat_creation_number", "application_token"], name: "messages_triple_index", unique: true, using: :btree
   end
 
 end

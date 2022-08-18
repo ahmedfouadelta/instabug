905e04a2c83dea4d7ccb8f2a317b42061f66f75a
Ahmed Fouad <ahmedfouadelta@gmail.com>
2022-08-18 17:28:24 +0200

    finalize index api for chats and messages and search api using like

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -125,10 +125,22 @@ class MessagesController < ApplicationController
       return render json: { error: "Chat's not found" }, status: 404 if chat.nil?
       return render json: { error: "No Messages were found for this Chat" }, status: 404 if chat.messages_count.zero?
 
+
+      database_messages = chat.messages
+      database_messages_ids = chat.messages.pluck(:message_number)
+      all_messages_ids = Array(1..chat.messages_count)
+      redis_messages_ids = all_messages_ids - database_messages_ids
       messages_arr = []
-      (1..chat.messages_count).each do |message_number|
-        message = MessageRepo.new.load_message(request.headers["TOKEN"], chat.chat_number, message_number)
+
+      database_messages.each do |message|
         messages_arr << {message_number: message.message_number, body: message.body}
+      end
+
+      redis_messages_ids.each do |message_number|
+        message = JSON.parse(
+          @redis.get("Message_#{request.headers["TOKEN"]}_#{message_params[:chat_number]}_#{message_number}")
+        )
+        messages_arr << {message_number: message["message_number"], body: message["body"]}
       end
 
       render(
@@ -141,41 +153,29 @@ class MessagesController < ApplicationController
     rescue
       render json: { error: "Something went wrong" }, status: 500
     end
-
   end
 
-  def search
-    app = Application.find_by(token: request.headers["TOKEN"]) 
-    if app.nil?
-      return render json: { error: "no app exists with this token" }, status: 404 
+  def search #done
+    begin
+      app = ApplicationRepo.new.load_app(request.headers["TOKEN"])
+      return render json: { error: "Application's not found" }, status: 404  if app.nil?
+
+      chat = ChatRepo.new.load_chat(request.headers["TOKEN"], message_params[:chat_number])
+      return render json: { error: "Chat's not found" }, status: 404 if chat.nil?
+      return render json: { error: "No Messages were found for this Chat" }, status: 404 if chat.messages_count.zero?
+
+      messages = chat.messages.where('body LIKE ?', "%#{params[:searched_text]}%")
+
+      render(
+        json: {
+          success: true,
+          messages: MessageSerializer.new(messages).to_h
+        },
+          status: :ok,
+      )
+    rescue
+      render json: { error: "Something went wrong" }, status: 500
     end
-
-    chat = app.chats.find_by(chat_number: message_params[:chat_number])
-    if chat.nil? && message_params[:chat_number] > app.chats_count
-      return render json: { error: "no chat exists with this number" }, status: 404
-    elsif chat.nil? && message_params[:chat_number] <= app.chats_count
-      return render json: { error: "no messages exist for this chat" }, status: 404
-    end
-
-    all_messages_numbers = Array(1..chat.messages_count)
-    created_messages_numbers = chat.messages.pluck(:chat_number)
-    not_created_yet_messages_numbers = all_messages_numbers - created_messages_numbers
-    arr_of_messages= MessageSerializer.new(chat.messages).to_h
-
-    redis = Redis.new(host: "host.docker.internal")
-
-    not_created_yet_messages_numbers.each do |message_number|
-      message_body = redis.get("#{request.headers["TOKEN"]}_#{message_params[:chat_number].to_s}_#{message_number.to_s}")
-      arr_of_messages.push({message_number: message_number, body: message_body})
-    end
-
-    render(
-      json: {
-        success: true,
-        messages: arr_of_messages.where("body like ?", "%#{message_params[:body]}%") 
-      },
-        status: :ok,
-    )
   end
 
   private
@@ -184,7 +184,3 @@ class MessagesController < ApplicationController
     params.require(:message).permit(:chat_number, :body, :message_number)
   end
 end
-
-# @redis.mapped_hmset("best3", message.attributes)
-#  @redis.hincrby("best3","chat_number", 1)
-# ap @redis.hgetall("best3").json?
